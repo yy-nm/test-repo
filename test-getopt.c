38a4677a456aa38825cd03dd034fe1dd91fd8abb
mardyu <michealyxd@hotmail.com>
2015-12-03 20:14:54 +0800

    add getopt demo

---
+++ test-getopt.c
@@ -0,0 +1,34 @@
+/*=============================================================================
+#     FileName: test-getopt.c
+#         Desc: simple demo for getopt func to parse args
+#       Author: mardyu
+#        Email: michealyxd@hotmail.com
+
+#      Version: 0.0.1
+#   LastChange: 2015-12-03 19:52:15
+#      History:
+=============================================================================*/
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+
+
+int main(int argc, char **args)
+{
+	int op;
+	/*
+	 * params:
+	 *	without arg: -a, -b -e
+	 *	with arg: -c, -d, -f
+	 * if character of optstring with ':'(colon) behind means params with 
+	 * arg
+	 *
+	 * */
+	while(0 <= (op = getopt(argc, args, "abc:d:ef:"))) {
+		fprintf(stdout, "key: %c value: %s\n", op, optarg);
+	}
+
+	return 0;
+}
